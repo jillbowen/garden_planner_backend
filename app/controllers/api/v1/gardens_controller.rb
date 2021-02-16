66fbd512b58073aa6725293aff02f0bb0d3a9460
jillbowen <jillian.b.short@gmail.com>
2021-02-15 16:53:54 -0800

    add index and create to garden controller

--- app/controllers/api/v1/gardens_controller.rb
+++ app/controllers/api/v1/gardens_controller.rb
@@ -1,6 +1,17 @@
 class Api::V1::GardensController < ApplicationController
 
     def index
-        
+        gardens = Garden.all 
+        render json: gardens
+    end
+
+    def create
+        garden = Garden.new(garden_params)
+    end
+
+    private
+
+    def garden_params
+        params.require(:garden).permit()
     end
 end
