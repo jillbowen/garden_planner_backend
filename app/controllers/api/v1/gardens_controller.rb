f788be1ae0cfed0f1153bc126c099ddcd6a8f179
jillbowen <jillian.b.short@gmail.com>
2021-02-15 18:06:42 -0800

    add garden index route and controller action

--- app/controllers/api/v1/gardens_controller.rb
+++ app/controllers/api/v1/gardens_controller.rb
@@ -2,16 +2,5 @@ class Api::V1::GardensController < ApplicationController
 
     def index
         gardens = Garden.all 
-        render json: gardens
+        render json: GardenSerializer.new(gardens)
     end
-
-    def create
-        garden = Garden.new(garden_params)
-    end
-
-    private
-
-    def garden_params
-        params.require(:garden).permit(:name)
-    end
-end
