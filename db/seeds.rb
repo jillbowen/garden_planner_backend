a0287886496a97784329a50a3efafea5f0031ff7
jillbowen <jillian.b.short@gmail.com>
2021-02-21 17:57:59 -0800

    update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,11 +10,11 @@ veggie = Garden.create(name: "Veggie")
 flower = Garden.create(name: "Flower")
 herb = Garden.create(name: "Herb")
 
-Plant.create(name: "Heirloom Tomato", description: "An heirloom tomato is an open-pollinated, non-hybrid heirloom cultivar of tomato.", 
-image_url: "https://gardenerspath.com/wp-content/uploads/2020/06/Best-Heirloom-Tomato-Varieties-FB.jpg", garden_id: veggie.id)
+Plant.create(name: "Heirloom Tomato", description: "Plant in full sun after danger of frost has passed, with a tomato cage or stake. Water in the morning, generously at first, then as needed.", 
+image_url: "https://www.almanac.com/sites/default/files/styles/primary_image_in_article/public/image_nodes/tomatoes-colorful_mementoimage-getty.jpg?itok=iGJ-LXBo", garden_id: veggie.id)
 
-Plant.create(name: "Hyacinth", description: "Hyacinths are perennial bulbs and are often planted directly in the ground outdoors, though these easy bulbs are also excellent for forcing in container gardens or being made to flower indoors during the winter.", 
-image_url: "https://upload.wikimedia.org/wikipedia/commons/7/7e/Hyacinth_-_Anglesey_Abbey.jpg", garden_id: flower.id)
+Plant.create(name: "Hyacinth", description: "Grow in well-drained soil in sun or partial shade. Make sure they are not watered too often. After flowering, cut back flower stalks and let the leaves die back naturally.", 
+image_url: "https://www.almanac.com/sites/default/files/styles/primary_image_in_article/public/image_nodes/hyacinths-marcin_jucha_ss.jpg?itok=LJX2n3zX", garden_id: flower.id)
 
-Plant.create(name: "Rosemary", description: "Rosemary is a perennial evergreen shrub with blue flowers. It is an aromatic and distinctive herb with a sweet, resinous flavor.", 
-image_url: "https://images.immediate.co.uk/production/volatile/sites/10/2018/02/f54e4ef5-157a-4e5a-ae7c-98f1b6898811-227de73.jpg?quality=90&resize=768%2C574", garden_id: herb.id)
+Plant.create(name: "Rosemary", description: "Plant in full sun and well drained soil with space to grow. Prune regularly, and water evenly, but be careful not to overwater.", 
+image_url: "https://www.almanac.com/sites/default/files/styles/primary_image_in_article/public/image_nodes/rosemary-foliage.jpg?itok=PR05PK0N", garden_id: herb.id)
